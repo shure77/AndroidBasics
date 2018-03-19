ed1986174365bf3a1604a426eb1c119a056fea1f
DESKTOP-ENKK7LO\Roman <r.groiss18@gmail.com>
2018-03-19 16:35:57 +0100

    Pull

--- MyFirstGoogleApp/app/src/main/java/com/example/myfirstgoogleapp/MainActivity.java
+++ MyFirstGoogleApp/app/src/main/java/com/example/myfirstgoogleapp/MainActivity.java
@@ -6,8 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
-
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
